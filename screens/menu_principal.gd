2479433be8ea6ddeed109491b1ed551dd2539ba4
Yago <yagoslima@outlook.com>
2026-07-13 22:54:13 -0300

    Add Godot project, assets and menu scenes

---
+++ screens/menu_principal.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Botao jogar
+func _on_btn_jogar_pressed():
+	get_tree().change_scene_to_file("res://screens/Batalha.tscn")
+
+# Botao sobre
+func _on_btn_sobre_pressed():
+	get_tree().change_scene_to_file("res://screens/menu_sobre.tscn")
+
+# Botao sair
+func _on_btn_sair_pressed():
+	get_tree().quit()
